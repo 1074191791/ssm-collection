4ad7059527a648d3404719070dda0af8c0256aba
zhoushuo <1074191791@qq.com>
2019-08-09 10:33:34 +0800

    commit all mybatis-plus test

--- ssm-datasources/src/test/java/com/zhou/service/BigTest.java
+++ ssm-datasources/src/test/java/com/zhou/service/BigTest.java
@@ -10,12 +10,43 @@ public class BigTest {
 
 
     public static void main(String[] args) {
-        BigDecimal a = BigDecimal.ZERO;
+        /*BigDecimal a = BigDecimal.ZERO;
         BigDecimal b = new BigDecimal(123);
 
         a = a.add(b);
 
-        System.out.println(a);
+        System.out.println(a);*/
+
+        BigDecimal a1 = new BigDecimal("2.00");
+        BigDecimal b1 = new BigDecimal(2.00d);
+        BigDecimal c1 = new BigDecimal("2.00");
+        BigDecimal d1 = new BigDecimal(2.0d);
+
+        System.out.println(b1 == d1);
+        System.out.println(b1.equals(d1));
+        System.out.println(b1.compareTo(d1) == 0);
+
+        System.out.println("================================");
+
+        System.out.println(a1 == b1);
+        System.out.println(a1.equals(b1));
+        System.out.println(a1.compareTo(b1) == 0);
+
+        System.out.println("================================");
+
+        System.out.println(a1 == c1);
+        System.out.println(a1.equals(c1));
+        System.out.println(a1.compareTo(c1) == 0);
+
+
+
+
+        /*BigDecimal a2 = new BigDecimal("2");
+        BigDecimal b2 = new BigDecimal(2);
+        System.out.println(a2 == b2);*/
+
+
+
 
     }
 }
--- ssm-mybatis-plus/src/main/java/com/zhou/model/dto/UserDTO.java
+++ ssm-mybatis-plus/src/main/java/com/zhou/model/dto/UserDTO.java
@@ -3,10 +3,13 @@ package com.zhou.model.dto;
 import com.zhou.model.pojo.User;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 /**
  * @author zhous
  * @create 2019-07-06 13:57
  */
 @ToString
-public class UserDTO extends User {
+public class UserDTO extends User implements Serializable {
+
 }
